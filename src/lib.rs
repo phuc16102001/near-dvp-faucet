260aa5248eb94f12c43482c24e8a39bab22ff0ca
phuc16102001 <phuc16102001@gmail.com>
2022-06-02 16:22:40 +0700

    Update datatype for parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::U128;
 use near_sdk::{
     collections::LookupMap,
     env, ext_contract, near_bindgen,
     serde::{Deserialize, Serialize},
     AccountId, Balance, Gas, PanicOnDefault, Promise, PromiseOrValue,
 };
-use near_sdk::borsh::{self,BorshDeserialize,BorshSerialize};
 
 use types::StorageKey;
 mod types;
@@ -19,20 +20,20 @@ pub const FAUCET_CALLBACK_GAS: Gas = 10_000_000_000_000;
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct FaucetInfo {
-    pub current_shared_balance: Balance,
-    pub available_balance: Balance,
-    pub total_share_account: u64,
-    pub max_share_per_account: Balance,
+    pub current_shared_balance: U128,
+    pub available_balance: U128,
+    pub total_share_account: U128,
+    pub max_share_per_account: U128,
     pub is_paused: bool,
 }
 
 impl FaucetInfo {
     pub fn from(contract: &FaucetContract) -> Self {
         FaucetInfo {
-            current_shared_balance: contract.current_shared_balance,
-            available_balance: contract.available_balance,
-            total_share_account: contract.total_share_account,
-            max_share_per_account: contract.max_share_per_account,
+            current_shared_balance: U128(contract.current_shared_balance),
+            available_balance: U128(contract.available_balance),
+            total_share_account: U128(contract.total_share_account),
+            max_share_per_account: U128(contract.max_share_per_account),
             is_paused: contract.is_paused,
         }
     }
@@ -43,13 +44,13 @@ impl FaucetInfo {
 // Token transfering method of NEP141
 #[ext_contract(ext_ft_contract)]
 pub trait FungibleTokenCore {
-    fn ft_transfer(&mut self, receiver_id: AccountId, amount: Balance, memo: Option<String>);
+    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
 }
 
 // For callback after transfer token
 #[ext_contract(ext_self)]
 pub trait ExtFaucetContract {
-    fn ft_transfer_callback(&mut self, amount: Balance, account_id: AccountId);
+    fn ft_transfer_callback(&mut self, amount: U128, account_id: AccountId);
 }
 
 // For transfering balance share from owner
@@ -57,9 +58,9 @@ pub trait FungibleTokenReceiver {
     fn ft_on_transfer(
         &mut self,
         sender_id: AccountId,
-        amount: Balance,
+        amount: U128,
         msg: String,
-    ) -> PromiseOrValue<u128>;
+    ) -> PromiseOrValue<U128>;
 }
 
 // ======================================== Faucet contract =====================================
@@ -71,7 +72,7 @@ pub struct FaucetContract {
     pub ft_contract_id: AccountId,       // FT contract account
     pub current_shared_balance: Balance, // Shared tokens
     pub available_balance: Balance,      // Current available to faucet
-    pub total_share_account: u64,        // Number of sharing accounts
+    pub total_share_account: Balance,    // Number of sharing accounts
     pub accounts: LookupMap<AccountId, Balance>, // Balance of account which was achieved (no storage deposit)
     pub max_share_per_account: Balance,          // Max balance fauceting for each account
     pub is_paused: bool,                         // Contract status
@@ -115,21 +116,21 @@ impl FaucetContract {
     }
 
     #[payable]
-    pub fn faucet_token(&mut self, amount: Balance) -> Promise {
+    pub fn faucet_token(&mut self, amount: U128) -> Promise {
         assert!(
             env::attached_deposit() > 1,
             "Please deposit at least 1 yocto NEAR"
         );
         assert!(!self.is_paused, "Faucet is paused");
         assert!(
-            self.available_balance >= amount,
+            self.available_balance >= amount.0,
             "Not enough token to share"
         );
 
         let account_id = env::predecessor_account_id();
-        let account_balance = self.accounts.get(&account_id).unwrap_or_else(|| 0);
+        let account_balance: Balance = self.accounts.get(&account_id).unwrap_or_else(|| 0);
         assert!(
-            account_balance + amount <= self.max_share_per_account,
+            account_balance + amount.0 <= self.max_share_per_account,
             "Exceeded maximum amount"
         );
 
@@ -145,33 +146,34 @@ impl FaucetContract {
             amount,
             account_id.clone(),
             &env::current_account_id(), // Calling contract (Faucet contract)
-            0,                         // Deposit amount
-            FAUCET_CALLBACK_GAS,       // Gas amount
+            0,                          // Deposit amount
+            FAUCET_CALLBACK_GAS,        // Gas amount
         ))
     }
 
     // After transfering successfully
     #[private]
-    pub fn ft_transfer_callback(&mut self, amount: Balance, account_id: AccountId) {
+    pub fn ft_transfer_callback(&mut self, amount: U128, account_id: AccountId) {
         let mut account_balance = self.accounts.get(&account_id).unwrap_or_else(|| 0);
         if account_balance == 0 {
             self.total_share_account += 1;
         }
-        account_balance += amount;
+        account_balance += amount.0;
 
         self.accounts.insert(&account_id, &account_balance);
-        self.current_shared_balance += amount;
-        self.available_balance -= amount;
+        self.current_shared_balance += amount.0;
+        self.available_balance -= amount.0;
     }
 }
 
+#[near_bindgen]
 impl FungibleTokenReceiver for FaucetContract {
     fn ft_on_transfer(
         &mut self,
         sender_id: AccountId,
-        amount: Balance,
+        amount: U128,
         msg: String,
-    ) -> PromiseOrValue<u128> {
+    ) -> PromiseOrValue<U128> {
         assert_eq!(
             sender_id, self.owner_id,
             "Only owner can transfer to the faucet"
@@ -182,9 +184,15 @@ impl FungibleTokenReceiver for FaucetContract {
             "Only accept token from the correct FT"
         );
 
-        self.available_balance += amount;
-        env::log(format!("Receive {} tokens from {}", &amount, &sender_id).as_bytes());
+        self.available_balance += amount.0;
+        env::log(
+            format!(
+                "Receive {} tokens from {} with message \"{}\"",
+                &amount.0, &sender_id, &msg
+            )
+            .as_bytes(),
+        );
 
-        PromiseOrValue::Value(0)
+        PromiseOrValue::Value(U128(0))
     }
 }
